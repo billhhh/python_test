f9365165f114ec15a8ee594e74312d9bf8bc6f9e
Bill Wang <billhhh@163.com>
2017-08-29 14:25:48 +0800

    prime ok

--- prime/prime.py
+++ prime/prime.py
@@ -1,6 +1,6 @@
-def primes(int kmax):
-    cdef int n, k, i
-    cdef int p[1000]
+def primes(kmax):
+    def n,k,i
+    def p[1000]
     result = []
     if kmax > 1000:
         kmax = 1000
@@ -8,6 +8,10 @@ def primes(int kmax):
     n = 2
     while k < kmax:
         i = 0
+        # p[i]里面放的素数
+        # n不能被p[i]整除，看看i == k，如果相等，证明直到找到第k-1个素数，都找不到可以整除n的素数
+        # 则n为第k个素数
+        # n不能被小于他的所有素数整除
         while i < k and n % p[i] != 0:
             i = i + 1
         if i == k:
