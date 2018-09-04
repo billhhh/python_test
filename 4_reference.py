1ee474e74b9c7c64e26712066f6e77f37fa239ab
bill <371570273@qq.com>
2018-09-04 18:01:33 +0930

    python 都是传地址

---
+++ 4_reference.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def func(A):
+    A = 10
+
+def func_list(L):
+    L[0] = 5
+
+N = 20
+print(N)
+func(N)
+print(N)
+
+Ll = []
+Ll.append(1)
+print(Ll[0])
+func_list(Ll)
+print(Ll[0])
+
